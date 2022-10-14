61e85251390dab135106c76eacbb1b6012cc781a
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2022-10-14 11:29:15 +0530

    add card design

--- src/components/Package.js
+++ src/components/Package.js
@@ -4,18 +4,16 @@ const Package = ({ tour }) => {
     let { title, description, img, price } = tour
 
     return (
-
-        <div className='package'>
-            <div className='imageSection'>
-                <img src={`/images/Home/${img}`} alt={'test'} />
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <img className="w-full" src={`/images/Home/${img}`} alt="Sunset in the mountains" />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{title}</div>
+                <p className="text-gray-700 text-base">{description}</p>
             </div>
-            <div className='detailSection'>
-                <div className='tourName'>{title}</div>
-                <div className='tourDescription'>{description}</div>
-                <span>{price}</span>
+            <div className="px-6 pt-4 pb-2">
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{price}/=</span>
             </div>
         </div>
-
     )
 }
 
